8d1a4ca312be75784a9e59f466b8761fc61cd094
wodyddml2 <wodyddml2@naver.com>
2022-08-04 15:26:39 +0900

    loading 과 pagination 구현

--- TMDBProject/Movie/ViewController.swift
+++ TMDBProject/Movie/ViewController.swift
@@ -2,19 +2,25 @@ import UIKit
 
 import Alamofire
 import SwiftyJSON
+import JGProgressHUD
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var movieCollectionView: UICollectionView!
     
+    let hub = JGProgressHUD()
+    
     var movieList: [MovieInfo] = []
     var genreList: [Int: String] = [:]
+    var changePage = 1
+    var totalPage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         movieCollectionView.delegate = self
         movieCollectionView.dataSource = self
+        movieCollectionView.prefetchDataSource = self
         
         movieCollectionView.register(UINib(nibName: MovieCollectionViewCell.resuableIdentifier, bundle: nil), forCellWithReuseIdentifier: MovieCollectionViewCell.resuableIdentifier)
         
@@ -36,16 +42,19 @@ class ViewController: UIViewController {
     }
 
     func requestTMDB() {
-        let url = "\(EndPoint.tmdbURL)api_key=\(APIKey.TMDB)"
+        hub.show(in: view)
+        let url = "\(EndPoint.tmdbURL)api_key=\(APIKey.TMDB)&page=\(changePage)"
         let genreURL = "\(EndPoint.tmdbGenreURL)api_key=\(APIKey.TMDB)"
         
         AF.request(url, method: .get).validate(statusCode: 200...400).responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-//                print("JSON: \(json)")
+                print("JSON: \(json)")
                 
                 for movie in json["results"].arrayValue {
+                    self.totalPage = json["total_pages"].intValue
+                    // 서버 통신 할 때 이미지의 모든 변환을 이 안에서 하면 서버 통신이 느려진다. / URL타입변환까진 얼마 차이 안난다함. Data타입만 cell에서 해주자!
                     let imageURL = URL(string: "https://image.tmdb.org/t/p/w500\(movie["poster_path"].stringValue)")
                     let imageData = try? Data(contentsOf: imageURL!)
                     
@@ -60,9 +69,11 @@ class ViewController: UIViewController {
                     
                     self.movieList.append(movieData)
                 }
+                self.hub.dismiss(animated: true)
                 self.movieCollectionView.reloadData()
                 
             case .failure(let error):
+                self.hub.dismiss(animated: true)
                 print(error)
             }
             
@@ -112,7 +123,6 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
         }
         
         cell.cellStyle()
-       
         
         return cell
     }
@@ -123,4 +133,17 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
         
         return CGSize(width: width / 1.1, height: width * 1.1)
     }
+}
+
+extension ViewController: UICollectionViewDataSourcePrefetching {
+
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            if movieList.count - 1 == indexPath.item && movieList.count < totalPage {
+                changePage += 1
+                requestTMDB()
+            }
+        }
+    }
+
 }
